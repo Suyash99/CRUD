113907fec4e3d822651f1bb18070848663520d5c
Suyash Kumar <suyashkumar009@example.com>
2022-08-23 16:10:00 +0530

    Fixed Counter Id Number service

--- mongoOperations.js
+++ mongoOperations.js
@@ -1,27 +1,30 @@
-const mongoose = require("mongoose");
 const Student = require("./schema");
 
 //CRUD Operations
 const counterService = async () => {
-  let dbEntry = await Student.find({}).sort({ _id: 1 }).limit(10)
-  console.log(dbEntry)
-  return typeof dbEntry == "object" && dbEntry.length == 0 ? 1 : dbEntry?.[0]?.idNumber
-}
+  let dbEntry = await Student.find({}).sort({ _id: -1 }).limit(1);
+
+  return typeof dbEntry == "object" && dbEntry.length == 0
+    ? 1
+    : dbEntry?.[0]?.idNumber;
+};
 
 const createMongo = async (req, res) => {
-  const student = new Student(req.body);
-  let idCounter = await counterService()
+  let idCounter = await counterService();
 
   if (!idCounter) {
     return res.status(500).json({
       data: null,
-      error: "Some error getting idNumber Counter-",
-      status: 500
-    })
+      error: "Some error getting idNumber Counter- " + idCounter,
+      status: 500,
+    });
   }
 
   //Assigning Counter to idNumber
-  req.body.idNumber = idCounter
+  req.body.idNumber = idCounter + 1;
+
+  const student = new Student(req.body);
+
   student.save((err, body) => {
     if (err) {
       res.status(400).json({
@@ -29,13 +32,15 @@ const createMongo = async (req, res) => {
         error: err.message,
       });
     } else {
-      body.createdAt = undefined;
-      body.updatedAt = undefined;
-      body.__v = undefined;
-      body._id = undefined;
       res.status(200).json({
-        data: body,
+        data: {
+          idNumber: body.idNumber,
+          name: body.name,
+          lastName: body.lastName,
+          email: body.email,
+        },
         error: null,
+        status: 200,
       });
     }
   });
@@ -47,11 +52,13 @@ const findAlldMongo = (req, res) => {
       res.status(400).json({
         data: null,
         error: err,
+        status: 400,
       });
     } else {
       res.status(200).json({
         data: data,
         error: null,
+        status: 200,
       });
     }
   });
@@ -62,13 +69,15 @@ const readOneMongo = (req, res) => {
     if (err) {
       res.status(400).json({
         data: null,
-        error: err.message,
+        error: "Error reading idNumber- " + err.message,
+        status: 400,
       });
     } else {
       if (found == null) {
         return res.status(400).json({
           data: null,
-          error: req.query.id + " _id doesnt exist in DB!",
+          error: req.query.id + " Id Number doesnt exist in DB!",
+          status: 400,
         });
       }
       found._id = undefined;
@@ -78,6 +87,7 @@ const readOneMongo = (req, res) => {
       res.status(200).json({
         data: found,
         error: null,
+        status: 200,
       });
     }
   });
@@ -90,6 +100,7 @@ const updateMongo = (req, res) => {
     return res.status(400).json({
       data: null,
       error: "Query parameter is not provided!",
+      status: 400,
     });
   }
   const updatedVal = req.body;
@@ -99,6 +110,7 @@ const updateMongo = (req, res) => {
         res.status(400).json({
           data: null,
           error: err,
+          status: 400,
         });
       } else {
         studentVal._id = undefined;
@@ -108,6 +120,7 @@ const updateMongo = (req, res) => {
         res.status(200).json({
           data: studentVal,
           error: null,
+          status: 200,
         });
       }
     }
@@ -121,6 +134,7 @@ const deleteMongo = (req, res) => {
     res.status(400).json({
       data: null,
       error: "Query Parameter is not provided!",
+      status: 400,
     });
   }
   Student.deleteOne({ idNumber: _id }).exec((err, deleteRes) => {
@@ -128,11 +142,13 @@ const deleteMongo = (req, res) => {
       res.status(400).json({
         data: null,
         error: err,
+        status: 400,
       });
     } else {
       res.status(200).json({
         data: deleteRes,
         error: null,
+        status: 200,
       });
     }
   });
@@ -146,13 +162,13 @@ const existsInDBCheck = async (req, res, next) => {
         res.status(400).json({
           data: null,
           error: "Internal Server error- " + err.message,
-          status: 400
+          status: 400,
         });
       } else if (!found) {
         res.status(400).json({
           data: null,
           error: req.query.id + " id not found in DB!",
-          status: 400
+          status: 400,
         });
       } else {
         next();
@@ -162,6 +178,29 @@ const existsInDBCheck = async (req, res, next) => {
     res.status(400).json({
       data: null,
       error: "Query parameter id is not provided properly!",
+      status: 400,
+    });
+  }
+};
+
+const emailUniqueCheck = async (req, res, next) => {
+  //Deprecated
+  if (req.body.email) {
+    let entryCheck = await Student.findOne({ email: req.body.email });
+
+    if (entryCheck) {
+      return res.status(409).json({
+        data: null,
+        error: req.body.email + " mail already exists in db!",
+        status: 409,
+      });
+    }
+    next();
+  } else {
+    res.status(400).json({
+      data: null,
+      error: "Email is not provided in the mail body",
+      status: 400,
     });
   }
 };
@@ -172,5 +211,5 @@ module.exports = {
   findAlldMongo: findAlldMongo,
   readOneMongo: readOneMongo,
   updateMongo: updateMongo,
-  deleteMongo: deleteMongo
-}
+  deleteMongo: deleteMongo,
+};
